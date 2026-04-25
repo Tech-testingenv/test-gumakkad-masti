6952896d2df620535e4178b075ab2001a225dd9b
Princetech7618 <pirncerajput04@gmail.com>
2026-04-25 17:00:37 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,10 +4,10 @@ import emailjs from '@emailjs/browser'
 export const sendEmail = async (params) => {
   try {
     const res = await emailjs.send(
-      process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
-      process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+  process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+  process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
       params,
-      process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+  process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
     )
     return res
   } catch (error) {
